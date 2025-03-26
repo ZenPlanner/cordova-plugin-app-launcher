b5b4e5a3d6d9ef348d9e5fb40a54ac4e1713640f
Paras Anand <paras.anand@daxko.com>
2025-03-26 12:26:45 +0530

    1.0.0 - Updated launch method for ios to fix issue with opening other apps

--- src/ios/Launcher.m
+++ src/ios/Launcher.m
@@ -24,17 +24,26 @@
 
 - (void)launch:(CDVInvokedUrlCommand*)command {
 	NSDictionary* options = [command.arguments objectAtIndex:0];
-	CDVPluginResult * pluginResult = nil;
+	CDVPluginResult *pluginResult = nil;
 
 	if ([options objectForKey:@"uri"]) {
 		NSString *uri = [options objectForKey:@"uri"];
-		if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:uri]]) {
-			NSURL *launchURL = [NSURL URLWithString:uri];
-			[[UIApplication sharedApplication] openURL: launchURL];
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+		NSURL *launchURL = [NSURL URLWithString:uri];
+
+		if ([[UIApplication sharedApplication] canOpenURL:launchURL]) {
+			[[UIApplication sharedApplication] openURL:launchURL
+                                               options:@{}
+                                     completionHandler:^(BOOL success) {
+				CDVPluginResult *result;
+				if (success) {
+					result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+				} else {
+					result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Failed to open the URI."];
+				}
+				[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+			}];
 		} else {
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that uri."];
+			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that URI."];
 			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		}
 	} else {
